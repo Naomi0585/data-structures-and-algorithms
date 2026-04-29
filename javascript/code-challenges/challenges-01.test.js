d2eb2ee2ae0454250fcbc85f7c45c18ec9c462b0
Naomi Torres <ivanys.ntm@gmail.com>
2026-04-29 14:37:07 -0700

    challenge one pass

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -8,9 +8,17 @@ Write a function named `addOne` that takes an array of numbers, and returns a ne
 Use `forEach` to loop over the input array and work with each value.  Push the new value into a local array. Return the local array;
 ------------------------------------------------------------------------------------------------ */
 
-const addOne = (arr) => {
+function addOne(arr) {
+  let result = [];
+
+  arr.forEach(function(num) {
+    result.push(num + 1);
+
+  });
+
+  return result;
   // Solution code here...
-};
+}
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
